a53ab6caa9f837419fa17b15aab6beb44ead9a82
SergeySA12 <strenadkosa@mail.ru>
2023-09-25 17:47:52 +0300

    Sem3-4

--- tests/test/test_negative.py
+++ tests/test/test_negative.py
@@ -5,7 +5,7 @@ import yaml
 
 
 class TestNegative:
-    with open('config_user.yaml') as fy:
+    with open('config.yaml') as fy:
         data = yaml.safe_load(fy)
 
     def test_negative1(self, make_folder, clear_folder, make_files, create_bad_archive):
